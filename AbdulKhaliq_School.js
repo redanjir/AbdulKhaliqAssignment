2dd921ab85295b5f2aadc1d17002fca963151a36
redanjir <glock68bedok@gmail.com>
2024-11-06 11:24:12 +0800

    small edits

--- AbdulKhaliq_School.js
+++ AbdulKhaliq_School.js
@@ -16,40 +16,40 @@ const SchoolManagement = {
     //Add new instructor to list and return the new list
     addInstructor(instructorName, age){
         if(isNaN(age) ){ //If age is a not a number
-            return 'Error: Age is not valid';
+            return console.log('Error: Age is not valid');
         }else if(instructorList.find(e => e.instructorName === instructorName)){ //If instructorname exists in array
-            return 'Instructor already exists';
+            return console.log('Instructor already exists');
         }
         else{
             age = parseInt(age); 
             var instructor = {instructorName, age}
             instructorList.push(instructor);
-            return instructorList;
+            return console.log(instructorList);
         }
     },
 
     //Add new class to list and return the new list
     addClass(className){
         if(classList.find(e => e.className === className)){
-            return 'Error: ClassName already exists';
+            return console.log('Error: ClassName already exists');
         }else{
             classList.push({className});
-            return classList;
+            return console.log(classList);
         }
     },
 
     //Add new student to list and return the new list
     addStudent(studentName, age){
         if(isNaN(age)){ //If age is not a number
-            return 'Error: Age is not valid'
+            return console.log('Error: Age is not valid');
         }else if(studentList.find(e => e.studentName === studentName)){
-            return 'Error: Student already exists'
+            return console.log('Error: Student already exists');
         }
         else{
             age = parseInt(age); 
             var student = {studentName, age}
             studentList.push(student);
-            return studentList;
+            return console.log(studentList);
         }
     },
 
@@ -57,19 +57,19 @@ const SchoolManagement = {
     removeStudentByName(studentName){
         var index = studentList.indexOf(studentName);
         if(index != -1){ //Indexof returns -1 if it cant find from the list
-            return 'Error: Enter an existing studentName';
+            return console.log('Error: Enter an existing studentName');
         }else{
             studentList.splice(index,1);
-            return studentList;
+            return console.log(studentList);
         }
     },
 
     //Return the student object by index
     getStudentbyIndex(index){
         if(studentList[index] === undefined){ //If element in array does not exist
-            return 'Enter a valid index';
+            return console.log('Enter a valid index');
         }
-        return studentList[index];
+        return console.log(studentList[index]);
     }
 }
 
--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const SchoolManagement = require('./AbdulKhaliq_School.js');
 
 console.log("Adding new Instructor");
-console.log(SchoolManagement.addInstructor("bob", 30));
+SchoolManagement.addInstructor("bob", 30);
 console.log('-----------------------------------------');
 
 console.log("Adding new Student");
-console.log(SchoolManagement.addStudent("Max", "20"));
+SchoolManagement.addStudent("Max", "20");
 console.log('-----------------------------------------');
 
 console.log("Adding new Class");
-console.log(SchoolManagement.addClass("Introduction to webapi"));
+SchoolManagement.addClass("Introduction to webapi");
 console.log('-----------------------------------------');
 
 console.log("Removing exisitng student by name & return new studentList");
-console.log(SchoolManagement.removeStudentByName("Max"));
+SchoolManagement.removeStudentByName("Max");
 console.log('-----------------------------------------');
 
 console.log("Get student by index");
-console.log(SchoolManagement.getStudentbyIndex(0));
+SchoolManagement.getStudentbyIndex(0);
 console.log('-----------------------------------------');
